f3ba1023b86403f8c45131a50a9537dc6fb781fe
elliot2077 <alex.volcov42@gmail.com>
2023-11-15 13:30:56 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3ad6c7d8-dcea-4e6c-be86-e1dabbfa07aa';
+export const MODAL_CLOSE_SEC = 2.5;
